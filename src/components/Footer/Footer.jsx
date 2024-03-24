bd6295f4876727d86b7774a2bc0eb3802d2a79d8
sohail <sksohail.swaraj@gmail.com>
2024-03-24 15:36:11 +0530

    footer fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,19 +4,13 @@ import ItemsContainer from "./ItemsContainer";
 
 const Footer = () => {
   return (
-    <>
-      <footer className="bg-black text-white">
-        <ItemsContainer />
-        <div
-          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8
-      text-center pt-2 text-gray-400 text-sm pb-8"
-        >
-          <span>© 2024 Appy. All rights reserved.</span>
-        </div>
-      </footer>
-    </>
+    <footer className="bg-black text-white">
+      <ItemsContainer />
+      <div className="flex justify-center pt-2 text-gray-400 text-sm pb-8">
+        <span>© 2024 Appy. All rights reserved.</span>
+      </div>
+    </footer>
   );
 };
 
 export default Footer;
-
