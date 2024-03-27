88a429976d472d5838626fe1a1958c88d776455f
KritikaBindal07 <kirtibindalkb770@gmail.com>
2024-03-27 23:12:13 +0530

    modal implemented

--- src/components/Home/FeaturedCollection/Collections.jsx
+++ src/components/Home/FeaturedCollection/Collections.jsx
@@ -12,3 +12,4 @@ const Collections = () => {
 };
 
 export default Collections;
+
--- src/components/constants/HomeCollectionData.jsx
+++ src/components/constants/HomeCollectionData.jsx
@@ -1,36 +1,46 @@
-import collection1 from "../../assets/collection1.jpg"
-import collection2 from "../../assets/collection2.jpg"
+import collection1 from "../../assets/collection1.jpg";
+import collection2 from "../../assets/collection2.jpg";
 import collection3 from "../../assets/collection3.jpg";
 import collection4 from "../../assets/collection4.jpg";
 
 export const collectionData = [
-    {
-      title: "image1",
-      url: collection1,
-    },
-    
-    {
-        title: "image2",
-        url: collection4,
-      },
+  {
+    title: "Product 1",
+    image: collection1,
+    description: "lorem Lorem ipsum dolor, sit amet consectetur",
+    price:"750"
+  },
 
+  {
+    title: "Product 2",
+    image: collection4,
+    description: "Lorem ipsum dolor sit amet.",
+    price:"980"
+  },
 
-    {
-      title: "image3",
-      url: collection2,
-    },
-    {
-      title: "image4",
-      url: collection3,
-    },
-    
-    {
-      title: "image5",
-      url:collection4 ,
-    },
-    {
-        title: "image6",
-        url:collection1 ,
-      },
-  ];
-  
+  {
+    title: "Product 3",
+    image: collection2,
+    description: "Lorem ipsum dolor loremhfs",
+    price:"999"
+  },
+  {
+    title: "Product 4",
+    image: collection3,
+    description: "Lorem ipsum dolor sit amet.",
+    price:"1090"
+  },
+
+  {
+    title: "Product 5",
+    image: collection4,
+    description: "Lorem ipsum dolor sit.",
+    price:"350"
+  },
+  {
+    title: "Product6",
+    image: collection1,
+    description: "Lorem ipsum dolor sit amet.",
+    price:"350"
+  },
+];
