74b9b0b25542ede61ed713aa7d9b544d1f8b3665
sohail <sksohail.swaraj@gmail.com>
2024-04-13 14:04:42 +0530

    homepage improved

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -1,88 +1,106 @@
 // FeaturedProducts.jsx
-import React from 'react';
-import feature1 from "../../assets/feature1.jpg";
-import feature2 from "../../assets/feature2.jpg";
-import feature3 from "../../assets/feature3.jpg";
+import React, { useState, useEffect } from 'react';
+import seamlessImage1 from "../../assets/feature1.jpg";
+import seamlessImage2 from "../../assets/feature1.jpg";
+import seamlessImage3 from "../../assets/feature13.jpg";
+import imprintsImage1 from "../../assets/feature2.jpg";
+import imprintsImage2 from "../../assets/feature21.jpg";
+import imprintsImage3 from "../../assets/feature22.jpg";
+import frenchTerryImage1 from "../../assets/feature3.jpg";
+import frenchTerryImage2 from "../../assets/feature31.jpg";
+import frenchTerryImage3 from "../../assets/feature32.jpg";
 import { FaArrowRight } from "react-icons/fa";
 
 const FeaturedProducts = () => {
- return (
-   <section className="flex items-center justify-center w-full overflow-hidden">
-     <div className="flex w-full">
-       <div className="w-6/12 m-0 p-0">
-         {/* Show feature1 image */}
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-         <div className="relative h-[60vh] w-full flex items-end justify-end"
-           style={{
-             backgroundImage: `url(${feature1})`,
-             backgroundSize: "cover",
-             backgroundPosition: "center",
-           }}>
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % 3);
+    }, 3000);
 
-           <button className="pl-8 mr-8 mb-8 z-10 text-black text-3xl font-base bg-white rounded-full hover:scale-[1.05] transition duration-300 animate-slide-up flex items-center">
-             Know more
-             <div className="h-10 w-10 bg-black text-white m-1 ml-6 rounded-full flex justify-center items-center rotate-[-45deg] hover:rotate-0 transition duratuion-75">
-               <FaArrowRight className="text-2xl" />
-             </div>
-           </button>
-           <h2 className='block absolute h-full w-full flex items-center justify-center font-heading text-7xl text-white'>
-             Seamless
-           </h2>
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
-         </div>
+  const seamlessImages = [seamlessImage1, seamlessImage2, seamlessImage3];
+  const imprintsImages = [imprintsImage1, imprintsImage2, imprintsImage3];
+  const frenchTerryImages = [frenchTerryImage1, frenchTerryImage2, frenchTerryImage3];
 
-         {/* Show feature2 image */}
-         <div className="relative h-[80vh] w-full flex items-end justify-end"
-           style={{
-             backgroundImage: `url(${feature2})`,
-             backgroundSize: "cover",
-             backgroundPosition: "center",
-           }}>
+  return (
+    <section className="flex items-center justify-center w-full overflow-hidden">
+      <div className="flex flex-col md:flex-row w-full">
+        <div className="w-full md:w-6/12 m-0 p-0">
+          {/* Show seamless images */}
+          <div className="relative h-[50vh] md:h-[60vh] w-full flex items-end justify-end">
+            {seamlessImages.map((image, index) => (
+              <div
+                key={index}
+                className={`absolute top-0 left-0 w-full h-full bg-cover bg-center transition-opacity duration-1000 ease-in-out ${
+                  index === currentImageIndex ? 'opacity-100' : 'opacity-0'
+                }`}
+                style={{ backgroundImage: `url(${image})` }}
+              />
+            ))}
+            <button className="pl-8 mr-8 mb-8 z-10 text-black text-xl md:text-3xl font-base bg-white rounded-full hover:scale-[1.05] transition duration-300 animate-slide-up flex items-center">
+              Know more
+              <div className="h-8 w-8 md:h-10 md:w-10 bg-black text-white m-1 ml-4 md:ml-6 rounded-full flex justify-center items-center rotate-[-45deg] hover:rotate-0 transition duration-75">
+                <FaArrowRight className="text-lg md:text-2xl" />
+              </div>
+            </button>
+            <h2 className="block absolute h-full w-full flex items-center justify-center font-heading text-4xl md:text-7xl text-white">
+              Seamless
+            </h2>
+          </div>
 
+          {/* Show imprints images */}
+          <div className="relative h-[50vh] md:h-[80vh] w-full flex items-end justify-end">
+            {imprintsImages.map((image, index) => (
+              <div
+                key={index}
+                className={`absolute top-0 left-0 w-full h-full bg-cover bg-center transition-opacity duration-1000 ease-in-out ${
+                  index === currentImageIndex ? 'opacity-100' : 'opacity-0'
+                }`}
+                style={{ backgroundImage: `url(${image})` }}
+              />
+            ))}
+            <button className="pl-8 mr-8 mb-8 z-10 text-black text-xl md:text-3xl font-base bg-white rounded-full hover:scale-[1.05] transition duration-300 animate-slide-up flex items-center">
+              Know more
+              <div className="h-8 w-8 md:h-10 md:w-10 bg-black text-white m-1 ml-4 md:ml-6 rounded-full flex justify-center items-center rotate-[-45deg] hover:rotate-0 transition duration-75">
+                <FaArrowRight className="text-lg md:text-2xl" />
+              </div>
+            </button>
+            <h2 className="block absolute h-full w-full flex items-center justify-center font-heading text-4xl md:text-7xl text-white">
+              Imprints
+            </h2>
+          </div>
+        </div>
 
-
-
-           <button className="pl-8 mr-8 mb-8 z-10 text-black text-3xl font-base bg-white rounded-full hover:scale-[1.05] transition duration-300 animate-slide-up flex items-center">
-             Know more
-             <div className="h-10 w-10 bg-black text-white m-1 ml-6 rounded-full flex justify-center items-center rotate-[-45deg] hover:rotate-0 transition duratuion-75">
-               <FaArrowRight className="text-2xl" />
-             </div>
-           </button>
-           <h2 className='block absolute h-full w-full flex items-center justify-center font-heading text-7xl text-white'>
-             Imprints
-           </h2>
-
-
-         </div>
-
-       </div>
-       {/* Show feature3 image */}
-       <div className="relative w-6/12 flex items-end justify-end"
-         style={{
-           backgroundImage: `url(${feature3})`,
-           backgroundSize: "cover",
-           backgroundPosition: "center",
-         }}>
-
-
-
-
-         <button className="pl-8 mr-8 mb-8 z-10 text-black text-3xl font-base bg-white rounded-full hover:scale-[1.05] transition duration-300 animate-slide-up flex items-center">
-           Know more
-           <div className="h-14 w-14 bg-black text-white m-1 ml-4 rounded-full flex justify-center items-center rotate-[-45deg] hover:rotate-0 transition duratuion-75">
-             <FaArrowRight className="text-2xl" />
-           </div>
-         </button>
-
-         <h2 className='block absolute h-full w-full flex items-center justify-center font-heading text-7xl text-white'>
-           French Terry
-         </h2>
-
-
-       </div>
-     </div>
-   </section>
- );
+        {/* Show French Terry images */}
+        <div className="relative w-full md:w-6/12 h-[50vh] md:h-auto flex items-end justify-end">
+          {frenchTerryImages.map((image, index) => (
+            <div
+              key={index}
+              className={`absolute top-0 left-0 w-full h-full bg-cover bg-center transition-opacity duration-1000 ease-in-out ${
+                index === currentImageIndex ? 'opacity-100' : 'opacity-0'
+              }`}
+              style={{ backgroundImage: `url(${image})` }}
+            />
+          ))}
+          <button className="pl-8 mr-8 mb-8 z-10 text-black text-xl md:text-3xl font-base bg-white rounded-full hover:scale-[1.05] transition duration-300 animate-slide-up flex items-center">
+            Know more
+            <div className="h-10 w-10 md:h-14 md:w-14 bg-black text-white m-1 ml-4 rounded-full flex justify-center items-center rotate-[-45deg] hover:rotate-0 transition duration-75">
+              <FaArrowRight className="text-lg md:text-2xl" />
+            </div>
+          </button>
+          <h2 className="block absolute h-full w-full flex items-center justify-center font-heading text-4xl md:text-7xl text-white">
+            French Terry
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FeaturedProducts;
